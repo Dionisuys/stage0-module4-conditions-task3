f9bb0e48e7ca8b38c1b6e04db3be0e13ed2a1d78
Dzianis Panasiuk <dzianispanasiuk@gmail.com>
2023-05-01 00:47:41 +0300

    updated 2 classes

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -3,15 +3,12 @@ package school.mjc.stage0.conditions.task3;
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
         if (salary >= 1 && salary <= 10000) {
-            salary = (int) (salary * 0.85);
-            System.out.println(salary);
+            System.out.println(salary * 0.85);
         } else if (salary <= 20000) {
-            salary = (int) (salary * 0.82);
-            System.out.println(salary);
+            System.out.println(salary * 0.82);
         } else if (salary > 20000) {
-            salary = (int) (salary * 0.8);
-            System.out.println(salary);
-        } else {
+            System.out.println(salary * 0.8);
+        } else if (salary <= 0){
             System.out.println("wrong input!");
         }
     }
